092093abccf7d4236e9517477ec0b5270d56be51
david444456 <38369752+david444456@users.noreply.github.com>
2021-06-03 18:23:21 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ajedrezLite.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "../ajedrezLite.exe"
+  "../ajedrezLite.exe.manifest"
+  "../ajedrezLite.pdb"
+  "../libajedrezLite.dll.a"
+  "CMakeFiles/ajedrezLite.dir/auxiliares.obj"
+  "CMakeFiles/ajedrezLite.dir/ejercicios.obj"
+  "CMakeFiles/ajedrezLite.dir/main.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej1_posicionValidaTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej2_posicionInicialValidaTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej3_casillasAtacadasTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej4_posicionSiguienteCorrectaTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej5_ordenarTableroTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej6_finDeLaPartidaTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej7_hayJaqueDescubiertoTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej8_ejecutarSecuenciaForzadaTEST.obj"
+  "CMakeFiles/ajedrezLite.dir/tests/ej9_seVieneElJaqueEnTEST.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ajedrezLite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
