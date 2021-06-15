98b9815bb5db7d09361950f3146857a8c26b7a50
david444456 <38369752+david444456@users.noreply.github.com>
2021-06-15 17:14:29 -0300

    Test 2, 5, fix error in exercise 5

--- tests/ej5_ordenarTableroTEST.cpp
+++ tests/ej5_ordenarTableroTEST.cpp
@@ -57,4 +57,81 @@ TEST(ordenarTableroTEST, yaEstabaOrdenado){
     EXPECT_EQ(tableroActual(p), t_ord);
 }
 
+TEST(ordenarTableroTEST, PrimerasYUltimasFilasLlenas){
+    tablero t = {
+            {cTORRE_N,  cALFIL_N,     cREY_N,   cALFIL_N,    cTORRE_N ,    cTORRE_N ,   cTORRE_N,    cTORRE_N },
+            {cPEON_N,   cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cPEON_B,   cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B},
+            {cTORRE_B,  cPEON_B,     cALFIL_B,   cPEON_B,     cREY_B,     cALFIL_B,   cPEON_B,     cTORRE_B},
+    };
+    tablero t_ord = {
+            {cALFIL_N,  cALFIL_N,     cTORRE_N,   cTORRE_N,     cTORRE_N,   cTORRE_N,   cTORRE_N,     cREY_N},
+            {cPEON_N,   cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cPEON_B,   cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B},
+            {cPEON_B,  cPEON_B,     cPEON_B,  cALFIL_B ,     cALFIL_B,   cTORRE_B,   cTORRE_B,     cREY_B},
+    };
+    posicion p = make_pair(t,BLANCO);
+    ordenarTablero(p);
+    EXPECT_EQ(tableroActual(p), t_ord);
+}
+
+TEST(ordenarTableroTEST, PrimerasYUltimasFilasLlenasColoresMixtos){
+    tablero t = {
+            {cTORRE_N,  cALFIL_N,     cREY_N,   cALFIL_B,    cTORRE_N ,    cTORRE_N ,   cTORRE_N,    cTORRE_N },
+            {cPEON_N,   cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cPEON_B,   cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B},
+            {cTORRE_B,  cPEON_B,     cALFIL_N,   cPEON_B,     cREY_B,     cALFIL_B,   cPEON_B,     cTORRE_B},
+    };
+    tablero t_ord = {
+            {cALFIL_B,  cALFIL_N,     cTORRE_N,   cTORRE_N,     cTORRE_N,   cTORRE_N,   cTORRE_N,     cREY_N},
+            {cPEON_N,   cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA,     cVACIA},
+            {cPEON_B,   cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B},
+            {cPEON_B,  cPEON_B,     cPEON_B,  cALFIL_B,    cALFIL_N,   cTORRE_B,   cTORRE_B,     cREY_B},
+    };
+    posicion p = make_pair(t,BLANCO);
+    ordenarTablero(p);
+    EXPECT_EQ(tableroActual(p), t_ord);
+}
+
+TEST(ordenarTableroTEST, TodasLasFilasConPiezasColoresMixtos){
+    tablero t = {
+            {cTORRE_N,  cALFIL_N,     cREY_N,   cALFIL_B,    cTORRE_N ,    cTORRE_N ,   cTORRE_N,    cTORRE_N },
+            {cPEON_N,   cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cPEON_N,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cPEON_N,     cVACIA,     cVACIA,     cREY_B,     cVACIA,     cPEON_N,     cVACIA},
+            {cVACIA,    cALFIL_B,     cVACIA,     cVACIA,     cVACIA,     cPEON_N,     cVACIA,     cVACIA},
+            {cTORRE_B,  cPEON_B,     cALFIL_N,   cREY_B,     cREY_N,     cALFIL_B,   cREY_B,     cTORRE_B},
+            {cPEON_B,   cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B},
+            {cTORRE_B,  cPEON_B,     cALFIL_N,   cPEON_B,     cREY_B,     cALFIL_B,   cPEON_B,     cTORRE_B},
+    };
+    tablero t_ord = {
+            {cALFIL_B,  cALFIL_N,     cTORRE_N,   cTORRE_N,     cTORRE_N,   cTORRE_N,   cTORRE_N,     cREY_N},
+            {cPEON_N,   cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N,    cPEON_N},
+            {cVACIA,    cVACIA,     cVACIA,     cVACIA,     cPEON_N,     cVACIA,     cVACIA,     cVACIA},
+            {cVACIA,    cPEON_N,     cVACIA,     cVACIA,     cPEON_N,     cVACIA,     cREY_B,     cVACIA},
+            {cVACIA,    cPEON_N,     cVACIA,     cVACIA,     cVACIA,     cALFIL_B,     cVACIA,     cVACIA},
+            {cPEON_B ,  cALFIL_B,     cALFIL_N,   cTORRE_B,    cTORRE_B ,   cREY_B,  cREY_B,     cREY_N},
+            {cPEON_B,   cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B,    cPEON_B},
+            {cPEON_B,  cPEON_B,     cPEON_B,  cALFIL_B,    cALFIL_N,   cTORRE_B,   cTORRE_B,     cREY_B},
+    };
+    posicion p = make_pair(t,BLANCO);
+    ordenarTablero(p);
+    EXPECT_EQ(tableroActual(p), t_ord);
+}
 
